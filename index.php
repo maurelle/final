438b194ffa9c1c2a98d2ee9586da5efe1a2b020b
maurelle <pk.ap099@gmail.com>
2020-12-02 11:20:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-
 $all_music = file_get_contents("https://dd-wtlab2020.netlify.app/pre-final/ezquiz.json");
-
 $musics = json_decode($all_music, true);
-
 $musics = $musics['tracks']['items'];
-
 ?>
 
 <!DOCTYPE html>
@@ -17,14 +13,11 @@ $musics = $musics['tracks']['items'];
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-
     <div class="container">
         <div class="row">
             <?php
-            
             foreach($musics as $music) {
                 ?>
-
                 <div class="col-md-4 mt-3">
                     <div class="card">
                         <img class="card-img-top" src="<?php echo $music['album']['images'][0]['url'] ?>" alt="">
@@ -47,11 +40,9 @@ $musics = $musics['tracks']['items'];
                 
                 <?php
             }
-
             ?>
         </div>
     </div>
-    
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 </body>
 </html>
